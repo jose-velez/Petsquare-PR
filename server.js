fa40e623a061090ebd9de420df715756eca1ed0e
Jose Velez <jose.velez363@gmail.com>
2018-01-19 22:04:56 -0500

    Adding the server and dependencies.

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//==================================
+//            ROUTES
+//==================================
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//================================
+//              PORT
+//================================
+
+app.listen(PORT, function() {
+  console.log(`App running on Port: ${PORT}`);
+});
